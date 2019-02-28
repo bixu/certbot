d04d43d0b0a107179772c7256b49ac7673efebf7
Blake Irvin <blakeirvin@me.com>
2019-02-28 15:55:40 +0100

    0.1.0 (#1)
    * move towards supporting a full range of plugins
    * avoid errors around non-artifact-creation in the interactive Studio
    * attempt renewal only daily

---
+++ plan.sh
@@ -0,0 +1,77 @@
+pkg_name=certbot
+pkg_origin=bixu
+pkg_maintainer='Blake Irvin <blakeirvin@me.com>'
+pkg_license=('Apache-2.0')
+pkg_description='The Certbot LetsEncrypt client.'
+pkg_build_deps=(
+  'bixu/cacher'
+)
+
+pkg_deps=(
+  'core/bash'
+  'core/python'
+)
+pkg_plugins=(
+  'dns-cloudflare'
+  'dns-cloudxns'
+  'dns-digitalocean'
+  'dns-dnsimple'
+  'dns-dnsmadeeasy'
+  'dns-gehirn'
+  'dns-google'
+  'dns-linode'
+  'dns-luadns'
+  'dns-nsone'
+  'dns-ovh'
+  'dns-rfc2136'
+  'dns-route53'
+  'dns-sakuracloud'
+)
+pkg_bin_dirs=(bin)
+pkg_svc_user='root'  #TODO: determine security standards for generated certs
+
+pkg_version() {
+  pip --disable-pip-version-check search "$pkg_name" \
+    | grep "^$pkg_name " \
+    | cut -d'(' -f2 \
+    | cut -d')' -f1
+}
+
+do_before() {
+  update_pkg_version
+}
+
+do_prepare() {
+  python -m venv "$pkg_prefix"
+  source "$pkg_prefix/bin/activate"
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  for plugin in ${pkg_plugins[@]}
+  do
+    pip --disable-pip-version-check install "$pkg_name-$plugin==$pkg_version"
+  done
+}
+
+do_after() {
+  if [ $HAB_CREATE_PACKAGE == 'false' ]
+  then
+    build_line "WARN: Skipping artifact creation because 'HAB_CREATE_PACKAGE=false'"
+
+    _generate_artifact() {
+      return 0
+    }
+
+    _prepare_build_outputs() {
+      return 0
+    }
+  fi
+}
+
+do_strip() {
+  return 0
+}
