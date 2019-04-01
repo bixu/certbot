6e52d017bd4d3fbb17323cda0dc5225febff27bb
Blake Irvin <blakeirvin@me.com>
2019-04-01 20:45:26 +0200

    adopt ye olde Makefile

---
+++ tests/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+hab pkg install 'core/bats'
+hab pkg install 'core/busybox-static'
+hab pkg install 'core/hab-sup'
+
+export PATH="$PATH:$(hab pkg path 'core/bats')/bin"
+export PATH="$PATH:$(hab pkg path 'core/busybox-static')/bin"
+
+until hab svc status &> '/dev/null'
+do
+  hab sup run --no-color > 'nohup.out' &
+  sleep 3
+done
+
+source '/src/.studiorc'
+
+svc load 'certbot'
+svc load 'nginx'
+
+until hab svc status 'bixu/nginx' | grep -v 'down' &> '/dev/null'
+do
+  sleep 1
+done
+
+sleep 5
+
+tail '/hab/sup/default/sup.log' | grep 'certbot.default'
+
+bats ./tests/*.bats
